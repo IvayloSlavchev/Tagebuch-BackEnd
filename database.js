85cc8beaec012ad5450c8f7e3c43f6b9ed75784d
IvayloSlavchev <ivaylo.slavchev124@gmail.com>
2023-01-27 17:56:05 +0200

    Removed Dockerfile and added password validation

--- database.js
+++ database.js
@@ -2,12 +2,19 @@ const mysql = require('mysql2');
 const dotenv = require('dotenv');
 dotenv.config();
 
-module.exports = mysql.createPool({
+let db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT
 });
+db.connect(function(error) {
+    if(error){
+        return console.error('Error: ' + error);
+    }
 
+    console.log("Connected to database");
+})
 
+module.exports = db;
