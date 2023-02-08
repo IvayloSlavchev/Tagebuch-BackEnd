4f625c493ef9107e923371dc9854914c2cb94fb0
IvayloSlavchev <ivaylo.slavchev124@gmail.com>
2023-02-08 21:33:56 +0200

    Destroying connection on every query

--- Users/users.js
+++ Users/users.js
@@ -14,54 +14,61 @@ router.use((req, res, next) => {
 router.post('/registration', async (req, res) => {
     let { username, email, phone, password, role, schoolName } = req.body;
 
-    const passwordHash = await bcrypt.hash(password, 10);
+        const passwordHash = await bcrypt.hash(password, 10);
 
 
-    if (username && email && phone && password && role && schoolName) {
-        try {
-            const isValid = await connection.promise().query(`SELECT * FROM users WHERE username=?`, [username])
+        if (username && email && phone && password && role && schoolName) {
+            try {
+                const isValid = await connection.promise().query(`SELECT * FROM users WHERE username=?`, [username])
 
-            if (isValid[0].length === 0) {
-                await connection.promise().query(`INSERT INTO users(username, email, phone, password, role, schoolName) 
+                if (isValid[0].length === 0) {
+                    await connection.promise().query(`INSERT INTO users(username, email, phone, password, role, schoolName) 
                             VALUES(?, ?, ?, ?, ?, ?)`, [username, email, phone, passwordHash, role, schoolName]);
-                res.status(201).json({ msg: 'Created' })
+                    res.status(201).json({ msg: 'Created' })
+                    connection.destroy();
+                    return;
+                }
+
+                res.status(400).json({ msg: 'User already exists' })
+                connection.destroy();
+                return;
+            } catch (error) {
+                res
                 connection.destroy();
                 return;
             }
-
-            res.status(400).json({ msg: 'User already exists' })
-            connection.destroy();
-            return;
-        } catch (error) {
-            console.log('An error occured when trying to register: ' + error);
-            connection.destroy();
-            return;
         }
-    }
-})
+    } 
+)
 
 
 router.post('/login', async (req, res) => {
     let { username, email, password } = req.body;
 
     try {
-        
+
         const doesUserExist = await connection.promise().query(`SELECT * FROM users WHERE email=?`, [email]);
-        
+
         if (doesUserExist[0].length === 0) {
-            return res.status(404).json({ msg: 'User not found' })
+            res.status(404).json({ msg: 'User not found' })
+            connection.destroy();
+            return;
         }
-        
+
         const hashedPassword = doesUserExist[0].map(item => item.password);
         const doesUserProvideCorrectPassword = await bcrypt.compare(password.toString(), hashedPassword.toString());
 
-        if(doesUserProvideCorrectPassword === false) {
-            return res.status(409).json({ msg: 'Incorrect password' })
+        if (doesUserProvideCorrectPassword === false) {
+            res.status(409).json({ msg: 'Incorrect password' })
+            connection.destroy();
+            return;
         }
 
-        return res.status(200).json({ msg: 'Logged in' })
+       res.status(200).json({ msg: 'Logged in' })
+       connection.destroy();
     } catch (error) {
-        return res.status(400).json({ msg: 'Error occured while trying to login: ' + error });
+         res.status(400).json({ msg: 'Error occured while trying to login: ' + error });
+         connection.destroy();
     }
 })
 module.exports = router
